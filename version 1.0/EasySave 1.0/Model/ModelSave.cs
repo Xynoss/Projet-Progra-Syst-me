0d7db38cd0f7fdc8a47eabe7b13a5d60ec85932e
Xynoss <74651875+Xynoss@users.noreply.github.com>
2021-11-23 15:50:21 +0100

    modif qlqconque

--- version 1.0/EasySave 1.0/Model/ModelSave.cs
+++ version 1.0/EasySave 1.0/Model/ModelSave.cs
@@ -1,7 +1,17 @@
 using System;
 
+/// <summary>
+/// class for a model of the save to create a save with a name, source path, and a target path
+/// </summary>
+/// <autor>
+/// THOMAS Maxime
+/// </autor>
 namespace Model {
 	public abstract class ModelSave {
+
+		/// <summary>
+		/// name of the save definition
+		/// </summary>
 		private string name;
 		public string Name {
 			get {
@@ -11,6 +21,10 @@ namespace Model {
 				name = value;
 			}
 		}
+
+		/// <summary>
+		/// source path definition
+		/// </summary>
 		private string sourcePath;
 		public string SourcePath {
 			get {
@@ -20,6 +34,10 @@ namespace Model {
 				sourcePath = value;
 			}
 		}
+
+		/// <summary>
+		/// target path definition
+		/// </summary>
 		private string targetPath;
 		public string TargetPath {
 			get {
@@ -30,9 +48,21 @@ namespace Model {
 			}
 		}
 
-		public void ModelSave(ref string name, ref string sourcePath, ref string targetPath) {
-			throw new System.NotImplementedException("Not implemented");
+		/// <summary>
+		/// creator of the class ModelSave which save the target and source path plus the name of the save
+		/// </summary>
+		/// <param name="name">name of the save</param>
+		/// <param name="sourcePath">source path of the file</param>
+		/// <param name="targetPath">target path of the save</param>
+		public ModelSave(ref string name, ref string sourcePath, ref string targetPath) {
+			this.TargetPath = targetPath;
+			this.SourcePath = sourcePath;
+			this.Name = name;
 		}
+
+		/// <summary>
+		/// 
+		/// </summary>
 		public abstract void Save();
 
 		private MapperClass.MapperLog mapperLog;
--- version 1.0/EasySave 1.0/Model/ModelTotalSave.cs
+++ version 1.0/EasySave 1.0/Model/ModelTotalSave.cs
@@ -2,7 +2,25 @@ using System;
 
 namespace Model {
 	public class ModelTotalSave : ModelSave  {
-		public override void Save() {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="sourcePath"></param>
+        /// <param name="targetPath"></param>
+        public ModelTotalSave(ref string name, ref string sourcePath, ref string targetPath) : base(ref name, ref sourcePath, ref targetPath)
+        {
+            try
+            {
+
+            }catch
+            {
+
+            }
+        }
+
+        public override void Save() {
 			throw new System.NotImplementedException("Not implemented");
 		}
 
