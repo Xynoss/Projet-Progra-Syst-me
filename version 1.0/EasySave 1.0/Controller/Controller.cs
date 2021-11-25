6013fe2a76ea342a11ffdf0671c82ea979636725
Xynoss <74651875+Xynoss@users.noreply.github.com>
2021-11-25 21:54:43 +0100

    rédaction et interaction avec la console

--- version 1.0/EasySave 1.0/Controller/Controller.cs
+++ version 1.0/EasySave 1.0/Controller/Controller.cs
@@ -45,35 +45,103 @@ namespace EasySave_1_0.Controller
         }
         public void Start()
         {
-
-
-            this.view.Output("que faire :");
-            string Input_choise = this.view.Input();
-            /*switch (Input_choise != null)
-            {
-                case false:
-                    Input_choise = 
-            }*/
-
-            this.view.Output("créer une sauvegarde ? y => YES ");
+            string Input_choise = "";
+            string save_name = "";
+            int save_selected = 0;
+            this.view.Output(this.res_man.GetString("greeting", this.culture));
+            this.view.Output(this.res_man.GetString("language_choose", this.culture));
+            this.view.Output(this.res_man.GetString("language_en", this.culture));
+            this.view.Output(this.res_man.GetString("language_fr", this.culture));
             Input_choise = this.view.Input();
-            if (Input_choise == "y" && saves.Count < 6)
+
+            switch (Input_choise)
             {
-                this.view.Output("le nom :");
-                string save_name = this.view.Input();
-                this.view.Output("la source :");
-                string save_sourcepath = this.view.Input();
-                this.view.Output("la cible :");
-                string save_targetpath = this.view.Input();
-                saves.Add(new model.ModelTotalSave(save_name, save_sourcepath, save_targetpath));
-                saves[0].Save();
-            }
-            else
-            {
-                this.view.Output("tu ne peux pas créer !");
+                case "1":
+                    culture = CultureInfo.CreateSpecificCulture("en");
+                    this.view.Output(this.res_man.GetString("check_language", this.culture));
+                    break;
+                case "2":
+                    culture = CultureInfo.CreateSpecificCulture("fr");
+                    this.view.Output(this.res_man.GetString("check_language", this.culture));
+                    break;
             }
 
+            this.view.Output(this.res_man.GetString("choose_action", this.culture));
+            this.view.Output(this.res_man.GetString("list_create_save", this.culture));
+            this.view.Output(this.res_man.GetString("list_delete_save", this.culture));
+            this.view.Output(this.res_man.GetString("list_run_all", this.culture));
+            this.view.Output(this.res_man.GetString("list_run_one", this.culture));
+            this.view.Output(this.res_man.GetString("list_exit", this.culture));
+            Input_choise = this.view.Input();
 
+
+
+
+            switch (Input_choise)
+            {
+                case "1": //create back_up
+                    this.view.Output(this.res_man.GetString("save_create_chosen", this.culture));
+                    Input_choise = this.view.Input();
+                    if (Input_choise == "y" && saves.Count < 6)
+                    {
+                        this.view.Output(this.res_man.GetString("ask_name_save", this.culture));
+                        save_name = this.view.Input();
+                        this.view.Output(this.res_man.GetString("ask_source_path", this.culture));
+                        string save_sourcepath = this.view.Input();
+                        this.view.Output(this.res_man.GetString("ask_target_path", this.culture));
+                        string save_targetpath = this.view.Input();
+                        saves.Add(new model.ModelTotalSave(save_name, save_sourcepath, save_targetpath));
+                        saves[0].Save();
+                    }
+                    else
+                    {
+                        this.view.Output(this.res_man.GetString("ask_target_save", this.culture));
+                    }
+                    break;
+
+                case "2": //delete backup
+                    this.view.Output(this.res_man.GetString("choose_save", this.culture));
+                    Input_choise = this.view.Input();
+
+                    try
+                    {
+                        int i = 0;
+                        while (Input_choise != save_name)
+                        {
+                            save_name = saves[i].Name;
+                            i++;
+                        }
+                        save_selected = i;
+                    }
+                    catch
+                    {
+
+                    }
+
+                    this.view.Output(string.Format(this.res_man.GetString("confirm_delete", this.culture), save_name));
+                    Input_choise = this.view.Input();
+                    if (Input_choise == "y")
+                    {
+                        Saves.Remove(saves[save_selected]);
+                        this.view.Output(string.Format(this.res_man.GetString("delete_end", this.culture), save_name));
+                        save_name = "";
+                    }
+                    else
+                    {
+                        this.view.Output(this.res_man.GetString("cancel_delete", this.culture));
+                    }
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+            }
+            this.view.Output(this.res_man.GetString("end", this.culture));
 
         }
         public void LogState(ref EasySave_1_0.model.ModelSave[] save)
