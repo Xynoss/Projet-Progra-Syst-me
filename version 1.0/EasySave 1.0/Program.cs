4abe53a8c4f81e2032a2c032052b171029845289
Xynoss <74651875+Xynoss@users.noreply.github.com>
2021-11-25 10:30:09 +0100

    correction package 2

--- version 1.0/EasySave 1.0/Program.cs
+++ version 1.0/EasySave 1.0/Program.cs
@@ -6,7 +6,7 @@ namespace EasySave_1_0
     {
         public static void Main()
         {
-            List<EasySave_1_0.Model.ModelSave> list_backup = new List<EasySave_1_0.Model.ModelSave>();
+            List<EasySave_1_0.model.ModelSave> list_backup = new List<EasySave_1_0.model.ModelSave>();
             View.View display = new View.View();
             Controller.Controller control = new Controller.Controller(list_backup, display, true);
         }
