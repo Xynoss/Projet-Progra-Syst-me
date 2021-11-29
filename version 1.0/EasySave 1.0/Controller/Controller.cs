9071f36611beec6692767a73e5095398c3b6f06f
TOE Cedric Lawanango <cedrictoe01@gmail.com>
2021-11-29 23:03:45 +0100

    commentaires et modifications dans la partie delete du controller
    j'ai changé le "if-else" en "if -if"

--- version 1.0/EasySave 1.0/Controller/Controller.cs
+++ version 1.0/EasySave 1.0/Controller/Controller.cs
@@ -61,15 +61,26 @@ namespace EasySave_1_0.Controller
         /// </summary>
         public void Start()
         {
+            //initilizing
             string Input_choise = "";
             string save_name = "";
             int save_selected = 0;
+
+
             this.view.Output(this.res_man.GetString("greeting", this.culture));
             this.view.Output(this.res_man.GetString("language_choose", this.culture));
             this.view.Output(this.res_man.GetString("language_en", this.culture));
             this.view.Output(this.res_man.GetString("language_fr", this.culture));
+
+
+        
+            //user's choice effect
+
             Input_choise = this.view.Input();
 
+
+
+            //language choosing
             switch (Input_choise)
             {
                 case "1"://choose the english language
@@ -81,6 +92,10 @@ namespace EasySave_1_0.Controller
                     this.view.Output(this.res_man.GetString("check_language", this.culture));
                     break;
             }
+
+
+            //other function of program
+
             while (Input_choise != "5")//5 is to exit the app
             {
                 this.view.Output(this.res_man.GetString("choose_action", this.culture));
@@ -100,12 +115,19 @@ namespace EasySave_1_0.Controller
                         this.view.Output(this.res_man.GetString("save_create_chosen", this.culture));
                         if (saves.Count < 6)
                         {
+                            //getting name of save
                             this.view.Output(this.res_man.GetString("ask_name_save", this.culture));
                             save_name = this.view.Input();
+
+                            //getting source path of save
                             this.view.Output(this.res_man.GetString("ask_source_path", this.culture));
                             string save_sourcepath = this.view.Input();
+
+                            //getting target path of save
                             this.view.Output(this.res_man.GetString("ask_target_path", this.culture));
                             string save_targetpath = this.view.Input();
+
+                            
                             saves.Add(new model.ModelTotalSave(save_name, save_sourcepath, save_targetpath));
                             model.ModelLogState statefile = new model.ModelLogState(save_name, save_sourcepath, save_targetpath);
                             this.view.Output(string.Format(this.res_man.GetString("save_loading", this.culture), save_name));
@@ -146,7 +168,8 @@ namespace EasySave_1_0.Controller
                             this.view.Output(string.Format(this.res_man.GetString("delete_end", this.culture), save_name));
                             save_name = "";
                         }
-                        else
+
+                        if (Input_choise == "n")
                         {
                             this.view.Output(this.res_man.GetString("cancel_delete", this.culture));
                         }
