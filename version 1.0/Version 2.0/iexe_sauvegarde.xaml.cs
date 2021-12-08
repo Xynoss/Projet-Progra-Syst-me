7af481033b9e42db230f7107fb834c5a4f35b26a
TOE Cedric Lawanango <cedrictoe01@gmail.com>
2021-12-08 19:28:24 +0100

    redimension
     des fenêtres

---
+++ version 1.0/Version 2.0/iexe_sauvegarde.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Version_2._0
+{
+    /// <summary>
+    /// Logique d'interaction pour iexe_sauvegarde.xaml
+    /// </summary>
+    public partial class iexe_sauvegarde : Window
+    {
+        public iexe_sauvegarde()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ version 1.0/Version 2.0/isaves_executed.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Version_2._0
+{
+    /// <summary>
+    /// Logique d'interaction pour isaves_executed.xaml
+    /// </summary>
+    public partial class isaves_executed : Window
+    {
+        public isaves_executed()
+        {
+            InitializeComponent();
+        }
+    }
+}
